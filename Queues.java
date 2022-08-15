af66b9faebc35570907d3300dbb3dce8045b8539
8099751086 <dilwarhussainit24@gmail.com>
2022-08-16 00:13:18 +0530

    Create A Queue

---
+++ Queues.java
@@ -0,0 +1,22 @@
+package com.stacksqueues;
+/**
+* DILWAR
+
+
+*/
+
+public class Queues<T> {
+	LinkedList<T> linkedList;
+
+	public Queues() {
+		this.linkedList = new LinkedList<>();
+	}
+
+	public void enqueu(T key) {
+		linkedList.add(key);
+	}
+
+	public void print() {
+		linkedList.display();
+	}
+}
